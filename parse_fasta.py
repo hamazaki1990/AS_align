66ae024ede111c39d08cd637b710741c4716a247
hamazaki <hamazaki@nemurinezumi.local>
2018-03-18 21:45:13 +0900

    parse

--- parse_fasta.py
+++ parse_fasta.py
@@ -1,24 +1,22 @@
 from Bio import SeqIO
 
+rec_iter = SeqIO.parse("ABtype_livingASs.fa", "fasta")
+outputf = "AS_30bps.fa"
+
 
 def AS_makefragment(outputf, start, fraglength):
-    with open(outputf, "w") as outfile:
-        while True:
-            try:
-                sat = next(rec_iter)
-            except StopIteration:
-                break
-            else:
-                seq = sat[start:start+fraglength]
-                SeqIO.write(seq, outfile, "fasta")
+    while True:
+        try:
+            sat = next(rec_iter)
+        except StopIteration:
+            break
+        else:
+            return sat[start:start+fraglength]
 
 
-i = 0
-while True:
-    if i > 150:
-        break
-    else:
-        rec_iter = SeqIO.parse("Btype_livingASs.fa", "fasta")
-        outputf = "Btype_"+str(i)+".fa"
-        AS_makefragment(outputf, i, 55)
-    i = i + 45
+with open(outputf, "w") as outfile:
+    i = 0
+    while True:
+        seq = AS_makefragment(outputf, i, 30)
+        SeqIO.write(seq, outfile, "fasta")
+    i = i + 10
